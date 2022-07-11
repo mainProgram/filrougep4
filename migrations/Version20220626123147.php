b3c08391957a19115382e8078e19753a79061cbb
mainProgram <fazeyfadiop@gmail.com>
2022-07-11 18:44:05 +0000

    test deploiement10

---
+++ migrations/Version20220626123147.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220626123147 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE boisson CHANGE id id INT NOT NULL');
+        $this->addSql('ALTER TABLE boisson ADD CONSTRAINT FK_8B97C84DBF396750 FOREIGN KEY (id) REFERENCES produit (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user CHANGE adresse adresse LONGTEXT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE boisson DROP FOREIGN KEY FK_8B97C84DBF396750');
+        $this->addSql('ALTER TABLE boisson CHANGE id id INT AUTO_INCREMENT NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE adresse adresse LONGTEXT DEFAULT NULL');
+    }
+}
